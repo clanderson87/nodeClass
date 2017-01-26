ed9a753997323408d2e482db57d0d92528dbf65e
Chris Anderson <cl.anderson8@gmail.com>
2017-01-26 16:49:20 -0600

    CRUD ops successful

--- foodtruck-api/src/controller/foodtruck.js
+++ foodtruck-api/src/controller/foodtruck.js
@@ -18,5 +18,53 @@ export default ({ config, db }) => {
     });
   });
 
+  //v1/foodtruck/
+  api.get('/', (req, res) => {
+    Foodtruck.find({}, (err, foodtrucks) => {
+      if(err) {
+        res.send(err);
+      }
+      res.json(foodtrucks);
+    });
+  });
+
+  //v1/foodtruck:id
+  api.get('/:id', (req, res) => {
+    Foodtruck.findById(req.params.id, (err, foodtruck) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(foodtruck);
+    });
+  });
+
+  //v1/foodtruck/:id
+  api.put('/:id', (req, res) => {
+    Foodtruck.findById(req.params.id, (err, foodtruck) => {
+      if (err){
+        res.send(err)
+      }
+      foodtruck.name = req.body.name;
+      foodtruck.save(err => {
+        if (err){
+          res.send(err)
+        }
+        res.json({ message: "Foodtruck saved successfully!" });
+      });
+    });
+  });
+
+  //v1/foodtruck/:id
+  api.delete('/:id', (req, res) => {
+    Foodtruck.remove({
+      _id: req.params.id
+    }, (err, foodtruck) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json({ message: "That foodtruck got got."});
+    });
+  })
+
   return api;
 }
