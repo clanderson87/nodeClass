200c2a07d6edcc3d28385a5576b44fb65ae4c6e3
Chris Anderson <cl.anderson8@gmail.com>
2017-02-07 18:33:19 -0600

    comments

--- tipTracker/src/controllers/account.js
+++ tipTracker/src/controllers/account.js
@@ -37,10 +37,6 @@ export default ({ config, deb }) => {
       session: false,
       failureRedirect: '/nope'
     }), generateAccessToken, respond);
-    //   (req, res) => {
-    //   console.log('After generateAccessToken, req.token is: ', req.token);
-    //   res.redirect("http://localhost:3005/v1/account/profile?access_token=" + req.token);
-    // });
 
   api.get('/logout', authenticate, (req, res) => {
     res.logout();
@@ -51,14 +47,14 @@ export default ({ config, deb }) => {
     res.status(200).json(req.user);
   });
 
-  api.get(
-    '/profile',
-    passport.authenticate('bearer', { session: false }),
-    function(req, res) {
-        console.log(req.user);
-        res.send("LOGGED IN as " + req.user.facebook.name + " - <a href=\"/logout\">Log out</a>");
-    }
-  );
+  // api.get(
+  //   '/profile',
+  //   passport.authenticate('bearer', { session: false }),
+  //   function(req, res) {
+  //       console.log(req.user);
+  //       res.send("LOGGED IN as " + req.user.facebook.name + " - <a href=\"/logout\">Log out</a>");
+  //   }
+  // );
 
   return api;
 }
